0ffe966de7ab048701531ce934bddcddbbbb345a
GuidoGabrielPascucci <g.g.pascucci@gmail.com>
2021-07-05 09:39:27 -0300

    Cambios Ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,7 +1,9 @@
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+
 function mostrar()
+/*
+Autor: Pascucci, Guido Gabriel
+Enunciado: Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".*/
+
 {
 	alert("Esto funciona de maravilla");
 }
